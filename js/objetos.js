817de1890a71de00291f522cafbac26fdb44d4dc
franmoyano <francoemmanuelmoyano@gmail.com>
2023-05-23 10:54:07 -0300

    TerceraPreEntrega

---
+++ js/objetos.js
@@ -0,0 +1,19 @@
+class Producto {
+    constructor(id, nombre, descripcion, precio, img, cantidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.img = img;
+        this.cantidad = cantidad = 1;
+    }
+}
+
+const juego1 = new Producto(1, "Call of Duty Modern Warfare 2 PS5", "Incluye - Lote multigeneración de Call of Duty® Modern Warfare® II", 1000, "img/juego1.jpg");
+const juego2 = new Producto(2, "God of War Ragnarök PS5", "Embárcate en un épico y cordial viaje junto a Kratos y Atreus", 1000, "img/juego2.jpg");
+const juego3 = new Producto(3, "NBA 2K23 PS5", "Ponte a la altura y desarrolla todo tu potencial en NBA 2K23", 600, "img/juego3.jpg");
+const juego4 = new Producto(4, "Elden Ring Edición Deluxe PS5", "Incluye juego completo, libro de arte digital y banda sonora original", 1200, "img/juego4.jpg");
+const juego5 = new Producto(5, "Grand Theft Auto V PS5", "Entra en las vidas de tres delincuentes muy distintos, Michael, Franklin y Trevor", 500, "img/juego5.jpg");
+const juego6 = new Producto(6, "FIFA 23 PS5", "Con los torneos de la Copa Mundial de la FIFA™ masculina y femenina", 900, "img/juego6.jpg");
+const juego7 = new Producto(7, "Horizon Forbidden West PS5", "Descubre tierras lejanas, enemigos nuevos, culturas ricas y personajes llenos de vida", 1100, "img/juego7.jpg");
+const juego8 = new Producto(8, "Cyberpunk 2077 PS4", "Es un RPG de aventura y acción de mundo abierto", 500, "img/juego8.jpg");
